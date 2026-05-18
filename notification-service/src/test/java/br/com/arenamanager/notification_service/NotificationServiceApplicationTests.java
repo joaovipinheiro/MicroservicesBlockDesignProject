852a1cedeb622ff2aecbf7a91fdf58f7cea40b25
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-05-17 21:45:56 -0300

    feat: adicionando microserviços restantes

---
+++ notification-service/src/test/java/br/com/arenamanager/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arenamanager.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
