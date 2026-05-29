565d4605dbb56098ac9fbcec7e7df174a0b62c54
Gabriel <gfmaurity@gmail.com>
2026-05-29 12:32:45 -0300

    feat: Inclusão do DTO TournamentCreatedEvent

--- tournament-service/src/main/java/br/com/arenamanager/tournament_service/Dto/TournamentCreatedEvent.java
+++ tournament-service/src/main/java/br/com/arenamanager/tournament_service/Dto/TournamentCreatedEvent.java
@@ -1,4 +1,14 @@
 package br.com.arenamanager.tournament_service.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TournamentCreatedEvent {
+    private Long idTorneio;
+    private String nome;
+    private String formato;
 }
