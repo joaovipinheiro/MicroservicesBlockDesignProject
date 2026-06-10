18decc9e1b5f028a0a4e3dd8956e9f94ce094202
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-06-10 17:45:07 -0300

    refactor(payment): ajusta estrutura do DTO de solicitação de pagamento

--- payment-service/src/main/java/br/com/arenamanager/payment_service/dto/SolicitacaoPagamentoDTO.java
+++ payment-service/src/main/java/br/com/arenamanager/payment_service/dto/SolicitacaoPagamentoDTO.java
@@ -1,6 +1,15 @@
 package br.com.arenamanager.payment_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import java.math.BigDecimal;
 
-public record SolicitacaoPagamentoDTO(Long usuarioId, Long torneioId, BigDecimal valor) {
-}
+public record SolicitacaoPagamentoDTO(
+
+        @JsonAlias({"usuarioId", "playerId"})
+        Long usuarioId,
+
+        @JsonAlias({"torneioId", "tournamentId"})
+        Long torneioId,
+
+        BigDecimal valor
+) {}
